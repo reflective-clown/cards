55ec7ee9b1db9b4093d7eb8e5412ef4c2242b3c9
reflective-clown <papacharliepapa@icloud.com>
2019-03-10 09:44:36 -0400

    disabled preventDefault on touchmove

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function init() {
 
         // card.addEventListener("touchend", handleStart, false);
         card.addEventListener("touchmove", function (evt) {
-            evt.preventDefault();
+            // evt.preventDefault();
             card.move = true;
         });
     });
