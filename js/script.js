073e538c5fb7b1b848aec1a91b8df004f06ffe52
reflective-clown <papacharliepapa@icloud.com>
2019-03-10 08:45:08 -0400

    resolved z-index

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
 
     init();
@@ -9,17 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function init() {
-    let els = document.getElementsByClassName("card");
-   // els.addEventListener("touchstart", handleStart, false);
+    const cards = document.getElementsByClassName("card");
 
-    Array.from(els).forEach( (el) => {
-         el.addEventListener("touchstart", handleStart, false);
-
+    Array.from(cards).forEach((card) => {
+        card.addEventListener("touchstart", handleStart, false);
     });
 
-    // el.addEventListener("touchend", handleEnd, false);
-    // el.addEventListener("touchcancel", handleCancel, false);
-    // el.addEventListener("touchmove", handleMove, false);
     console.log("initialized.");
 }
 
@@ -29,40 +21,3 @@ function handleStart(evt) {
     console.log("touchstart.")
 
 }
-
-//
-//
-// function registerListeners () {
-//     const tiles = document.getElementsByClassName("box");
-//
-//     Array.from(tiles).forEach((tile) => {
-//         tile.addEventListener('click', function () {
-//
-//             const active = tile.classList.contains("box-active");
-// // const top = window.pageYOffset;
-//             if (active) {
-//
-//
-//                 console.info("active....");
-//                 tile.style.transform = "translate(0px, 0px)";
-//
-//                 tile.classList.remove("box-active")
-//
-//             } else {
-//                 const style = window.getComputedStyle(tile);
-//                 const top = style.getPropertyValue('top');
-//                 console.log(top);
-//
-//                 tile.classList.add('box-active');
-//                 tile.style.transform = "translate(120px, 120px)";
-//                 // tile.style.top = top.toString();
-//                 // const tileTop = tile.style.top.toString();
-//                 const windowXPos = window.pageXOffset.toString();
-//                 const newTileTop = 0;
-//                 console.log("coords: ", top, " ", windowXPos);
-//
-//             }
-//
-//         });
-//     });
-// }
